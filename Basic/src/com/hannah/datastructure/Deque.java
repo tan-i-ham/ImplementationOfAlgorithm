d9f193d2aef748a2b32e5c04b2d46784d193edb0
Chen, Yi-Han a <yihan.a.chen@rakuten.com>
2019-08-29 18:00:31 +0900

    deque init

--- Basic/src/com/hannah/datastructure/Deque.java
+++ Basic/src/com/hannah/datastructure/Deque.java
@@ -1,17 +1,37 @@
-/*
- * Copyright (c) Rakuten, Inc. All Rights Reserved.
- *
- * This program is the information assets which are handled as "Strictly Confidential".
- * Permission of Use is only admitted in Rakuten Inc Development Department.
- * If you don't have permission,
- * MUST not be published, broadcast, rewritten for broadcast or publication or redistributed
- * directly or indirectly in any medium.
- *
- */
 package com.hannah.datastructure;
 
 /**
  * @author yihan.a.chen
  */
 public class Deque {
+    int mazSize;
+    long[] dequeArray;
+    int front;
+    int rear;
+
+    public Deque(int s) {
+        mazSize = s;
+        dequeArray = new long[mazSize];
+        front = 0;
+        rear = -1;
+    }
+
+    public void insertLeft(long num) {
+        dequeArray[front++] = num;
+        rear++;
+    }
+
+    public long removeLeft() {
+        rear--;
+        return dequeArray[front--];
+    }
+
+    public void insertRight(long num) {
+        dequeArray[++rear] = num;
+
+    }
+
+    public long removeRight() {
+        return dequeArray[rear--];
+    }
 }
