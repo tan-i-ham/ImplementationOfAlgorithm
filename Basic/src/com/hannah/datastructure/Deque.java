503306274e1dfba4c908b99d6342ed3328384b3f
tp6hannah <hannah51125@gmail.com>
2019-09-04 07:10:37 +0900

    deque 4 methods (not finished yet

--- Basic/src/com/hannah/datastructure/Deque.java
+++ Basic/src/com/hannah/datastructure/Deque.java
@@ -4,34 +4,76 @@ package com.hannah.datastructure;
  * @author yihan.a.chen
  */
 public class Deque {
-    int mazSize;
+    int maxSize;
     long[] dequeArray;
     int front;
     int rear;
+    int nElement;
+
 
     public Deque(int s) {
-        mazSize = s;
-        dequeArray = new long[mazSize];
+        maxSize = s;
+        dequeArray = new long[maxSize];
         front = 0;
         rear = -1;
+        nElement = 0;
     }
 
     public void insertLeft(long num) {
-        dequeArray[front++] = num;
-        rear++;
+        // deal with wraparound
+//        if (rear == maxSize - 1) {
+//            rear = -1;
+//        }
+        if (front < 0) {
+            front = maxSize - 1;
+        } else {
+            dequeArray[front--] = num;
+        }
+
+
+        nElement++;
     }
 
-    public long removeLeft() {
-        rear--;
-        return dequeArray[front--];
+    public void removeLeft() {
+        long numRemoved = dequeArray[front++];
+        System.out.println(String.format("%d be removed.", numRemoved));
+        nElement--;
     }
 
     public void insertRight(long num) {
+        // deal with wraparound
+        if (rear == maxSize - 1) {
+            rear = -1;
+        }
         dequeArray[++rear] = num;
-
+        nElement++;
     }
 
-    public long removeRight() {
-        return dequeArray[rear--];
+    public void removeRight() {
+        long numRemoved = dequeArray[rear--];
+        System.out.println(String.format("%d be removed.", numRemoved));
+        nElement--;
+    }
+
+    public String isEmpty() {
+        return nElement == 0 ? "Deque is empty" : "Deque is not empty";
+    }
+
+    public String isFull() {
+        return nElement >= maxSize ? "Deque is full" : "Deque is not full";
+    }
+
+    public void display() {
+        if (nElement == 0) {
+            System.out.println("The queue is empty now.");
+        } else {
+            System.out.print("[front -> rear]:");
+            for (int i = front; i < nElement; i++) {
+                System.out.print(dequeArray[i]);
+                
+            }
+
+            System.out.println("[end]");
+        }
     }
 }
--- Basic/src/com/hannah/datastructure/DequeApp.java
+++ Basic/src/com/hannah/datastructure/DequeApp.java
@@ -4,7 +4,33 @@ package com.hannah.datastructure;
  * @author yihan.a.chen
  */
 public class DequeApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Deque deque = new Deque(5);
 
+        deque.insertLeft(123);
+        deque.display();
+
+        deque.insertLeft(654);
+        deque.display();
+
+        deque.insertLeft(42);
+
+
+        deque.insertLeft(9);
+        deque.insertLeft(1);
+
+
+        deque.display();
+
+//        System.out.println(deque.isFull());
+
+        deque.removeLeft();
+
+        deque.display();
+        deque.insertLeft(333);
+
+
+//        deque.removeRight();
+        deque.display();
     }
 }
